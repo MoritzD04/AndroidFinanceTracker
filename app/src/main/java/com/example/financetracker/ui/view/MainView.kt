1a6775bd1d1942e6fbe56a2a080d8959f31b7bdd
MoritzD04 <moritz@familiedietrich.de>
2026-02-27 11:16:54 +0100

    Added FinanceEntries to MainView

--- app/src/main/java/com/example/financetracker/ui/view/MainView.kt
+++ app/src/main/java/com/example/financetracker/ui/view/MainView.kt
@@ -1,19 +1,27 @@
 package com.example.financetracker.ui.view
 
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.financetracker.ui.composable.BottomBar
+import com.example.financetracker.ui.composable.FinanceEntryCard
 import com.example.financetracker.ui.viewmodel.MainViewModel
 
+
 @Composable
-fun MainView(navController: NavHostController, viewModel: MainViewModel = viewModel()) {
+fun MainView(navController: NavHostController) {
+    val viewModel: MainViewModel = hiltViewModel()
+    val entries by viewModel.entries.collectAsState()
     Scaffold(
         bottomBar = { BottomBar(navController) }
     ) {
@@ -22,7 +30,17 @@ fun MainView(navController: NavHostController, viewModel: MainViewModel = viewMo
                 .fillMaxSize()
                 .padding(it)
         ) {
-            Text("ABC")
+            LazyColumn {
+                items(
+                    items = entries,
+                    key = { it.id }
+                ) { entry ->
+                    FinanceEntryCard(
+                        entry = entry,
+                        onDeleteClick = { viewModel.deleteEntry(entry) }
+                    )
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/financetracker/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/financetracker/ui/viewmodel/MainViewModel.kt
@@ -1,12 +1,42 @@
 package com.example.financetracker.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.financetracker.R
+import com.example.financetracker.behavior.command.AddFinanceEntryCommand
+import com.example.financetracker.behavior.command.Invoker
+import com.example.financetracker.behavior.command.RemoveFinanceEntryCommand
+import com.example.financetracker.model.FinanceEntry
+import com.example.financetracker.repository.IFinanceEntryRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-
+    private val financeEntryRepo: IFinanceEntryRepository
 ) : ViewModel() {
+    val invoker = Invoker()
 
+    val entries: StateFlow<List<FinanceEntry>> =
+        financeEntryRepo.getEntries()
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5000),
+                initialValue = emptyList()
+            )
+
+    init {
+        val cmd1 = AddFinanceEntryCommand(financeEntryRepo, FinanceEntry(
+            name = "Rewe",
+            description = "Einkauf",
+            value = 30.04f,
+            image = R.drawable.ic_launcher_foreground
+        ))
+        invoker.execute(cmd1)
+    }
+
+    fun deleteEntry(entry: FinanceEntry) = invoker.execute(RemoveFinanceEntryCommand(financeEntryRepo, entry))
 }
