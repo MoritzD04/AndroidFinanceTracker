777c1d35b444e3943bf1a1569f1e0b797a728ebb
MoritzD04 <moritz@familiedietrich.de>
2026-02-27 12:06:17 +0100

    Added Example Constants for FinanceEntry

---
+++ app/src/main/java/com/example/financetracker/util/TestConstants.kt
@@ -0,0 +1,19 @@
+package com.example.financetracker.util
+
+import com.example.financetracker.model.FinanceEntry
+import com.example.financetracker.R
+
+val FINANCE_ENTRY_1 = FinanceEntry(
+    name = "Rewe",
+    description = "Einkauf",
+    value = 30.04f,
+    image = R.drawable.ic_launcher_foreground
+)
+
+val FINANCE_ENTRY_2 = FinanceEntry(
+id = "1",
+name = "Edeka",
+description = "Einkauf",
+value = 21.01f,
+image = R.drawable.ic_launcher_foreground
+)
